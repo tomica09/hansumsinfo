34cfc1fc53da3125215639e5b6bc5ba8c2bf97ab
tomica09 <tomica09@naver.com>
2023-11-08 15:03:03 +0900

    D

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,18 @@
 </head>
 <body>
   <a href="/foodinfo">
-    <button>급식 정보</button>
+    <button id="fdinfo">급식 정보</button>
+    <img
+      src="http://hansu.ms.kr/object/lunch/images/sw13_lunch_imgBg.gif"
+      alt="급식정보"
+    />
   </a>
   <a href="/timestamp">
-    <button>시간표 정보</button>
+    <button id="tsinfo">시간표 정보</button>
   </a>
   <a href="http://hansu.ms.kr">
-    <button>학교 홈페이지로</button>
+    <button id="gotohansu">학교 홈페이지로</button>
   </a>
 </body>
+
+<style></style>
