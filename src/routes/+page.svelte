67f2752ff9f1f899b2482b542da099ff757b7204
tomica09 <tomica09@naver.com>
2023-11-08 15:36:05 +0900

    D

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,24 +2,36 @@
   <title>한수중학교 정보 안내</title>
 </head>
 <body>
-  <a href="/foodinfo">
-    <button id="fdinfo">급식 정보</button>
-  </a>
-  <a href="/timestamp">
-    <button id="tsinfo">시간표 정보</button>
-  </a>
-  <a href="http://hansu.ms.kr">
-    <button id="gotohansu">학교 홈페이지로</button>
-  </a>
-  <br />
-  <img
-    src="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA1MjhfMTQ2%2FMDAxNjUzNzExMjI0NDc3.5AcmpTKzGi5GcptgWkud6DiQbzmejHzQRPRuwn9Xqs4g.e0VsFy4XssQm8PYRt2r2ENfKf6Wb7T87fZkrLdNm5CYg.JPEG.food_jong%2FCK_ti195a12519.jpg&type=a340"
-    alt="급식정보"
-  />
-  <img
-    src="https://search.pstatic.net/sunny/?src=https%3A%2F%2Fkmug.co.kr%2Fdata%2Ffile%2Fdesign%2Fdata_illust_%25EC%258B%259C%25EA%25B0%2584%25ED%2591%259C_1.jpg&type=a340"
-    alt="시간표"
-  />
+  <div class="d">
+    <a href="/foodinfo">
+      <button class="food" id="fdinfo">급식 정보</button>
+    </a>
+
+    <img
+      class="food"
+      src="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA1MjhfMTQ2%2FMDAxNjUzNzExMjI0NDc3.5AcmpTKzGi5GcptgWkud6DiQbzmejHzQRPRuwn9Xqs4g.e0VsFy4XssQm8PYRt2r2ENfKf6Wb7T87fZkrLdNm5CYg.JPEG.food_jong%2FCK_ti195a12519.jpg&type=a340"
+      alt="급식정보"
+    />
+    <a href="/timestamp">
+      <button class="time" id="tsinfo">시간표 정보</button>
+    </a>
+    <img
+      class="time"
+      src="https://search.pstatic.net/sunny/?src=https%3A%2F%2Fkmug.co.kr%2Fdata%2Ffile%2Fdesign%2Fdata_illust_%25EC%258B%259C%25EA%25B0%2584%25ED%2591%259C_1.jpg&type=a340"
+      alt="시간표"
+    />
+    <a href="http://hansu.ms.kr">
+      <button id="gotohansu">학교 홈페이지로</button>
+    </a>
+    <br />
+  </div>
 </body>
 
-<style></style>
+<style>
+  .d {
+    display: inline;
+  }
+  .food {
+    display: inline-block;
+  }
+</style>
