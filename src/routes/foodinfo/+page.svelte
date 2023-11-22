9145577f4b8cdd2056ab87f039fe5d25bec5bc07
tomica09 <tomica09@naver.com>
2023-11-23 00:24:11 +0900

    d

--- src/routes/foodinfo/+page.svelte
+++ src/routes/foodinfo/+page.svelte
@@ -29,24 +29,19 @@ const fdinfo = async () => {
   let api_link =
     "https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7621035&key=052c429f8ab44a25b65f613ac02e8f90&type=json";
   let constant = "";
-  let Weekend = 0;
   // 날짜보정
   if (date.getDate() < 10) {
     constant = "0";
-  }
-  if (date.getDay() === 6) {
-    Weekend = 2;
-  } else if (date.getDay() === 0) {
-    Weekend = 1;
   }
   // 날짜 설정
   let today =
     String(date.getFullYear()) +
     String(date.getMonth() + 1) +
     constant +
-    String(date.getDate() + Weekend);
+    String(date.getDate());
   let link = api_link + "&MLSV_YMD=" + today;
-  dateinfo = today + "의 급식정보 입니다.";
+  
+  dateinfo = today + date.getVarDate() + "의 급식정보 입니다.";
   INFOMSG = "";
 
   // 함수
